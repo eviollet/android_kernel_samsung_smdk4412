e12ee9ad3ce3f04f81e510622959fa747caa23d9
Janson Kang <temasek71@gmail.com>
2012-07-23 10:10:33 +0800

    Fix compile error

--- drivers/video/samsung/s3cfb.h
+++ drivers/video/samsung/s3cfb.h
@@ -259,7 +259,7 @@ struct s3cfb_user_chroma {
 #define S3CFB_SET_ALPHA_MODE		_IOW('F', 313, unsigned int)
 
 extern struct fb_ops			s3cfb_ops;
-<<<<<<< HEAD
+
 /* extern inline struct s3cfb_global	*get_fimd_global(int id); */
 extern struct s3cfb_fimd_desc    *fbfimd;
 extern struct s3cfb_global	*get_fimd_global(int id);
